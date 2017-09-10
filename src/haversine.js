5eea4afaf4494b50401674401b2f3d2ba99df697
Avalander <darth.quesorico@gmail.com>
2017-09-10 18:09:16 +0200

    Move logic to transform-waypoints.js

--- src/haversine.js
+++ src/haversine.js
@@ -4,6 +4,7 @@ const to_radians = degrees => degrees * Math.PI / 180
 
 const square = x => x * x
 
+// I followed the implementation suggested in http://www.movable-type.co.uk/scripts/latlong.html
 const haversine = (a, b) => {
 	const a_latitude_rad = to_radians(a.latitude)
 	const b_latitude_rad = to_radians(b.latitude)
@@ -16,6 +17,9 @@ const haversine = (a, b) => {
 	return EARTH_RADIUS * central_angle
 }
 
+module.exports = haversine
+
+/*
 const law_of_cosines = (a, b) => {
 	const delta_lat = to_radians(b.latitude - a.latitude)
 	const delta_lon = to_radians(b.longitude - a.longitude)
@@ -34,30 +38,4 @@ const other = (a, b) => {
 	const central_angle = 2 * Math.asin(Math.sqrt(x))
 	return EARTH_RADIUS * central_angle
 }
-
-module.exports = {
-	haversine,
-	law_of_cosines,
-	other,
-}
-
-/*
-http://www.movable-type.co.uk/scripts/latlong.html
-
-a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
-c = 2 ⋅ atan2( √a, √(1−a) )
-d = R ⋅ c
-
-var R = 6371e3; // metres
-var φ1 = lat1.toRadians();
-var φ2 = lat2.toRadians();
-var Δφ = (lat2-lat1).toRadians();
-var Δλ = (lon2-lon1).toRadians();
-
-var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ/2) * Math.sin(Δλ/2);
-var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-var d = R * c;
 */
---
+++ src/transform-waypoints.js
@@ -0,0 +1,64 @@
+const make_waypoints_to_segments = calculate_distance => waypoints => {
+	const waypoints_with_date = waypoints
+		.map(x => Object.assign({}, x, { date: new Date(x.timestamp) }))
+	return waypoints_with_date
+		.slice(1, waypoints_with_date.length)
+		.reduce((acc, x) => ({
+			previous: x,
+			result: acc.result.concat({
+				duration: x.date - acc.previous.date,
+				distance: calculate_distance(x.position, acc.previous.position),
+				is_speeding: x.speed > x.speed_limit,
+			}),
+		}), {
+			previous: waypoints_with_date[0],
+			result: []
+		})
+		.result
+}
+
+const segments_reducer = ({
+	total_distance=0,
+	total_duration=0,
+	speeding_distance=0,
+	speeding_duration=0,
+}, x) => ({
+	total_duration: total_duration + x.duration,
+	speeding_duration: speeding_duration + (x.is_speeding ? x.duration : 0),
+	total_distance: total_distance + x.distance,
+	speeding_distance: speeding_distance + (x.is_speeding ? x.distance : 0),
+})
+
+const segments_to_categories = segments => segments.reduce(segments_reducer, {})
+
+const make_transform_waypoints = calculate_distance => waypoints => {
+	const segments = make_waypoints_to_segments(calculate_distance)(waypoints)
+	return segments_to_categories(segments)
+}
+
+module.exports = make_transform_waypoints
+
+
+/*
+const make_transform_waypoints = calculate_distance => waypoints => {
+	const waypoints_with_date = waypoints
+		.map(x => Object.assign({}, x, { date: new Date(x.timestamp) }))
+
+	const segments = waypoints_with_date
+		.slice(1, waypoints_with_date.length)
+		.reduce((acc, x) => ({
+			previous: x,
+			result: acc.result.concat({
+				duration: x.date - acc.previous.date,
+				distance: calculate_distance(x.position, acc.previous.position),
+				is_speeding: x.speed > x.speed_limit,
+			}),
+		}), {
+			previous: waypoints_with_date[0],
+			result: []
+		})
+		.result
+	
+	return segments.reduce(segments_reducer, {})
+}
+*/
